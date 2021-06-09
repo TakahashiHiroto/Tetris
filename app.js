ca8605a50ee77ed68f13de4ef865bed29aef0b7a
TakahashiHiroto <thirofoo@gmail.com>
2021-06-09 23:30:24 +0900

    add sqlite3

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 const express = require('express');
 const app = express();
+const sqlite3 = require('sqlite3');
 
 app.use(express.static('public'));
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
-  res.render('top.ejs');
+  var db = new sqlite3.Database('db.sqlite3');
+  db.serialize(function() {
+    db.all('SELECT id,name FROM USER', function(err, rows) {
+      const returndata = rows;
+      console.log(returndata);
+      res.render('top.ejs',{data:returndata});
+    });
+  });
+  db.close();
 });
 
+app.post('/post', (req, res) => {
+  var db = new sqlite3.Database('db.sqlite3');
+  const name = req.body['name'];
+  const id = req.body['id'];
+  console.log(name);
+  console.log(id);
+  db.run(
+      'insert into user (id,name) values (?,?)',
+      id,
+      name
+  );
+  res.redirect('/');
+});
 
 app.get('/tetris', (req,res) => {
   res.render('tetris.ejs');
 })
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8000);
